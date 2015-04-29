adc44254d0e3b696e2985ebe452679fdac327c66
Constantine Mars <constantine.mars@gmail.com>
2015-04-29 16:41:35 +0300

    bugs added

--- app/src/main/java/c/mars/dialogs/AlertDialogFragment.java
+++ app/src/main/java/c/mars/dialogs/AlertDialogFragment.java
@@ -13,6 +13,11 @@ import android.widget.TimePicker;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 import javax.xml.transform.Templates;
 
 import timber.log.Timber;
@@ -37,14 +42,46 @@ public class AlertDialogFragment extends DialogFragment{
                 .setMessage("message")
                 .setTitle("title")
                 .setIcon(android.R.drawable.ic_dialog_alert)
-                .setPositiveButton(ok, (dialog, which) -> {
-                    txt.setText(ok);
-                    Timber.d(ok);
+                .setPositiveButton(ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                txt.setText(ok);
+                                Timber.d(ok);
+                                System.gc();
+                            }
+                        }
+                )
+                .setNeutralButton("load", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        long sz = 4*1024*1024; //4MB
+                        txt.setText("load: + "+sz+"*"+Byte.SIZE);
+                        List<Byte> l = new LinkedList<>();
+                        Timber.d("empty sz: "+l.size());
+                        byte b;
+                        for (long i = 0; i < sz; i++) {
+                            b = (byte) i;
+                            l.add(b);
+                            if (i % 10_000 == 0) {
+                                Timber.d("" + i + ": " + b + " sz:"+l.size());
+                            }
+                        }
+                        txt.setText("load completed. sz:"+l.size()/(1024*Byte.SIZE)+"KB");
+                    }
                 })
-                .setNegativeButton(cancel, (d, w) -> {
-                    txt.setText(cancel);
-                    Timber.d(cancel);
-                })
+                .setNegativeButton(cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Integer n = null;
+                                Timber.d("crash now");
+
+                                Timber.d(n.toString());
+
+                                txt.setText(cancel);
+                                Timber.d(cancel);
+                            }
+                        }
+                )
                 .create();
     }
 }
